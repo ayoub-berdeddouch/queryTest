cd1ccc4ddf363228f7cf061a420fe35adf3a2a05
Ayoub BERDEDDOUCH <ayoub.berd@gmail.com>
2022-08-30 15:02:55 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -5,8 +5,7 @@ JOIN ref_niveauetude ON ref_niveauetude.id = bridge_studylevel_data.study_levelI
 JOIN bridge_sectors_data ON bridge_sectors_data.id = bridge_studylevel_data.idINNER 
 JOIN ref_secteurs ON ref_secteurs.idSecteur = bridge_sectors_data.sectorINNER 
 JOIN bridge_exp_data ON bridge_studylevel_data.id = bridge_exp_data.idINNER 
-JOIN ref_experience ON ref_experience.id = bridge_exp_data.exp_textand bridge_exp_data.exp_text = 3 
-					and bridge_studylevel_data.study_level= 11
+JOIN ref_experience ON ref_experience.id = bridge_exp_data.exp_textand bridge_exp_data.exp_text = 3 and bridge_studylevel_data.study_level= 11;
 
 
 
